c5f15261a06a2e7d6df0cc9cb844228c171b33ba
Iqbalaulia <16410100012@stikom.edu>
2020-07-14 23:03:33 +0700

    create page product ang register route page product

--- src/components/FooterHeyOutfit.vue
+++ src/components/FooterHeyOutfit.vue
@@ -5,14 +5,14 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-5">
-                    <div class="footer-left">
+                    <div class="footer-left text-left">
                         <div class="footer-logo">
                             <a href="#"><img src="img/logo_website_shayna_white.png" alt="" /></a>
                         </div>
                         <ul>
                             <li>Address: Setra Duta, Bandung</li>
                             <li>Phone: +628 22081996</li>
-                            <li>Email: hello.shayna@gmail.com</li>
+                            <li>Email: hello.heyoutfit@gmail.com</li>
                         </ul>
                         <div class="footer-social">
                             <a href="#"><i class="fa fa-facebook"></i></a>
@@ -23,7 +23,7 @@
                     </div>
                 </div>
                 <div class="col-lg-3 offset-lg-1">
-                    <div class="footer-widget">
+                    <div class="footer-widget text-left">
                         <h5>Information</h5>
                         <ul>
                             <li><a href="#">About Us</a></li>
@@ -34,7 +34,7 @@
                     </div>
                 </div>
                 <div class="col-lg-3">
-                    <div class="footer-widget">
+                    <div class="footer-widget text-left">
                         <h5>My Account</h5>
                         <ul>
                             <li><a href="#">My Account</a></li>
@@ -53,7 +53,7 @@
                         <div class="copyright-text">
                             Copyright &copy;
                             2020
-                            All rights reserved | Shayna
+                            All rights reserved | Hey.Outfit
                         </div>
                     </div>
                 </div>
---
+++ src/views/Product.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="product">
+    <HeaderHeyOutfit/>
+    <FooterHeyOutfit/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderHeyOutfit from '@/components/HeaderHeyOutfit.vue'
+import FooterHeyOutfit from '@/components/FooterHeyOutfit.vue'
+
+
+export default {
+  name: 'product',
+  components: {
+    HeaderHeyOutfit,
+    FooterHeyOutfit
+  }
+}
+</script>
