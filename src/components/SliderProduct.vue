e22c8ade668af2a2f33fb26e7c25afa2f5c6b1cc
Iqbalaulia <16410100012@stikom.edu>
2020-07-16 12:28:09 +0700

    implements axios at products slider heyoutfit

--- src/components/SliderProduct.vue
+++ src/components/SliderProduct.vue
@@ -4,9 +4,11 @@
     <section class="women-banner spad">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-12 mt-5">
+                
+                <div class="col-lg-12 mt-5" v-if="products.length > 0">
                     <carousel class="product-slider" :items="3" :nav="false" :dots="false"  :autoplay="true">
-                        <div class="product-item">
+                        
+                        <div class="product-item" v-for="itemProduct in products" v-bind:key="itemProduct.id">
                             <div class="pi-pic">
                                 <img src="/img/mickey1.jpg" alt="" />
                                 <ul>
@@ -19,81 +21,24 @@
                                 </ul>
                             </div>
                             <div class="pi-text">
-                                <div class="catagory-name">Coat</div>
+                                <div class="catagory-name">{{ itemProduct.type }}</div>
                                 <router-link to="/product">
-                                    <h5>Mickey Baggy</h5>
+                                    <h5>{{ itemProduct.name }}</h5>
                                 </router-link>
                                 <div class="product-price">
-                                    $14.00
+                                   {{ itemProduct.price }}
                                     <span>$35.00</span>
                                 </div>
                             </div>
                         </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-2.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Shoes</div>
-                                <a href="#">
-                                    <h5>Guangzhou sweater</h5>
-                                </a>
-                                <div class="product-price">
-                                    $13.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-3.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <a href="#">
-                                    <h5>Pure Pineapple</h5>
-                                </a>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-4.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                    <li class="w-icon">
-                                        <a href="#"><i class="fa fa-random"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <a href="#">
-                                    <h5>Converse Shoes</h5>
-                                </a>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
+                       
                     </carousel>
                 </div>
+
+                <div class="col-lg-12 mt-5" v-else>
+                    <p>Produk belum tersedia untuk saat ini</p>
+                </div>
+
             </div>
         </div>
     </section>
@@ -102,12 +47,29 @@
 </template>
 
 <script>
+// import owl carousel 
 import carousel from 'vue-owl-carousel';
+// import axios dari project
+import axios from "axios";
 
 export default {
     name: "SliderProduct",
     components:{
         carousel
+    },
+    // proses axios [0]
+    data(){
+         return{
+            products: []
+         };
+    },
+    // proses axios [1]
+    mounted(){
+        axios
+        .get("http://shayna-backend.belajarkoding.com/api/products")
+        .then(res => (this.products = res.data.data.data))
+        // disable eslint
+        .catch(err => console.log(err));
     }
 }
 </script>
